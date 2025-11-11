5857dbb6fb86fc777076cc028e51fae04c5fad2c
HugoWeijie <Hugo.ong@outlook.com>
2025-11-11 16:00:08 +0100

    study php

--- study.js
+++ study.js
@@ -1,341 +1,341 @@
-//all for studying purposes(nothing here is supposed to work)
-let _x = 5;
-const $y= 6;
-//const for constant values
-//let for consitant variable behavior
-//var not used because of scope confusion and is not needed to initilize
+// //all for studying purposes(nothing here is supposed to work)
+// let _x = 5;
+// const $y= 6;
+// //const for constant values
+// //let for consitant variable behavior
+// //var not used because of scope confusion and is not needed to initilize
 
 
-//---------------------------
-//JS operators, arithmetics and assignment pretty much the same as C++
-if(false){}else if(true){}else;
-switch(expression)
-{
-    case x:
-        break;
-    case y:
-        break;
-    default:
-}
-//JS if conditional, loops the same as well
-let text =(age <18)? "child": "adult";//same as java(ternary)
-//loops----------
-for(let i = 0; i< 10;i++){}//the same 
-//string search
-let text = "Please locate where 'locate' occurs!";
-let index = text.indexOf("locate");
-let index = text.lastIndexOf("locate");
-text.search("locate");
-//functions:
-function test()//standard
-{
-    return;
-}
-//arrow functions
-let myFunction = (a,b) => a *b;
-//==
-let myFunction2 = function(a, b) {return a * b}
-//objects
-const car = {type:"Fiat", model:"500", color:"white"};
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue"
-};
-console.log(person.age);
-//object methods
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  id: 5566,
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
-};
-//arrays
-const cars = ["Saab", "Volvo", "BMW"];
-//==
-const cars = [];
-cars[0]= "Saab";
-cars[1]= "Volvo";
-cars[2]= "BMW";
-let car = cars[0];
-//array methods are standard
-const numbers = [4, 9, 16, 25, 29];
-let first = numbers.find(myFunction);
+// //---------------------------
+// //JS operators, arithmetics and assignment pretty much the same as C++
+// if(false){}else if(true){}else;
+// switch(expression)
+// {
+//     case x:
+//         break;
+//     case y:
+//         break;
+//     default:
+// }
+// //JS if conditional, loops the same as well
+// let text =(age <18)? "child": "adult";//same as java(ternary)
+// //loops----------
+// for(let i = 0; i< 10;i++){}//the same 
+// //string search
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// let index = text.lastIndexOf("locate");
+// text.search("locate");
+// //functions:
+// function test()//standard
+// {
+//     return;
+// }
+// //arrow functions
+// let myFunction = (a,b) => a *b;
+// //==
+// let myFunction2 = function(a, b) {return a * b}
+// //objects
+// const car = {type:"Fiat", model:"500", color:"white"};
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+// console.log(person.age);
+// //object methods
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+// //arrays
+// const cars = ["Saab", "Volvo", "BMW"];
+// //==
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+// let car = cars[0];
+// //array methods are standard
+// const numbers = [4, 9, 16, 25, 29];
+// let first = numbers.find(myFunction);
 
-function myFunction(value, index, array) {
-  return value > 18;
-}
-//sort works for alphabet only
-//for numeric
-const points = [40, 100, 1, 5, 25, 10];
-points.sort(function(a, b){return b - a});
-//iterations
-array.forEach(element => {
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+// //sort works for alphabet only
+// //for numeric
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b){return b - a});
+// //iterations
+// array.forEach(element => {
     
-});
-const numbers2 = [45, 4, 9, 16, 25];
-let txt = "";
-numbers2.forEach(myFunction);
+// });
+// const numbers2 = [45, 4, 9, 16, 25];
+// let txt = "";
+// numbers2.forEach(myFunction);
 
-function myFunction(value, index, array) {
-  txt += value + "<br>";
-}
-const myArr = [1, 2, 3, 4, 5, 6];
-const newArr = myArr.flatMap((x) => x * 2);//the same as java
-const numbers3 = [45, 4, 9, 16, 25];
-let allOver18 = numbers3.every(myFunction);
-
-function myFunction(value, index, array) {
-  return value > 18;
-}
-const numbers4 = [45, 4, 9, 16, 25];
-let someOver18 = numbers4.some(myFunction);
-
-function myFunction(value, index, array) {
-  return value > 18;
-}
-//types arrays
-const myArr2 = new Uint8Array(5);
-const myArr3 = Uint8Array.of(0,1,2,3,4);
-//typed arrays to handle binary data
-const myArr4 = new Uint8ClampedArray(10);
-//sets only have unique values
-const letters = new Set(["a","b","c"]);
-const letters2 = new Set();
-letters instanceof Set;
-//maps
-const fruits = new Map([
-  ["apples", 500],
-  ["bananas", 300],
-  ["oranges", 200]
-]);
-//==
-const fruit2 = new Map();
-
-// Set Map Values
-fruits.set("apples", 500);
-fruits.set("bananas", 300);
-fruits.set("oranges", 200);
-fruits.get("apples");
-//events
-<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
-//function constructor
-const myFunction = new Function("a", "b", "return a * b");
-//hoisting
-myFunction(5);
-
-function myFunction(y) {
-  return y * y;
-}
-//self invoking functions
-(function () {
-  let x = "Hello!!";
-})();//functions are objects
-//this here is used as global object
-const person = {
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
-}
-const person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
-const person2 = {
-  firstName:"Mary",
-  lastName: "Doe"
-}
-
-person.fullName.call(person1);//call used to use a method belonging to another object
-const person = {
-  fullName: function(city, country) {
-    return this.firstName + " " + this.lastName + "," + city + "," + country;
-  }
-}
-
-const person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
-person.fullName.apply(person1);//takes argyments as array
-person.fullName.call(person1, "Oslo", "Norway");
-//objects 
-const person = new Object({
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue"
-});
-function Person(first, last, age, eye) {//same as java
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
-}
-//destructering
-let {firstName, lastName} = person;
-Object.assign(person1, person2);//iterations
-let text = Object.entries(person);//
-//getter and setter are the same as java
-class Car {
-  constructor(name, year) {
-    this.name = name;
-    this.year = year;
-  }
-}
-let myPromise = new Promise(function(myResolve, myReject) {//producing and consuming code
-  myResolve(); // when successful
-  myReject();  // when error
-});
-
-// "Consuming Code" (Must wait for a fulfilled Promise)
-myPromise.then(
-  function(value) { /* code if successful */ },
-  function(error) { /* code if some error */ }
-);
-//promise and async
-async function myFunction() {
-  return "Hello";
-}
-async function myFunction() {
-  return "Hello";
-}
-myFunction().then(
-  function(value) {myDisplayer(value);},
-  function(error) {myDisplayer(error);}
-);
-let value = await promise;//only able to be used in asynch
-async function myDisplay() {
-  let myPromise = new Promise(function(resolve, reject) {
-    resolve("!JOFJAISFJLDJ");
-  });
-  document.getElementById("demo").innerHTML = await myPromise;
-}
-
-myDisplay();
-async function myDisplay() {
-  let myPromise = new Promise(function(resolve) {
-    setTimeout(function() {resolve("DSASDSA");}, 3000);
-  });//timeout
-  document.getElementById("demo").innerHTML = await myPromise;
-}
-
-myDisplay();
-
-//The HTML elements as objects
-//The properties of all HTML elements
-//The methods to access all HTML elements
-//The events for all HTML element
-<html>
-<body>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = "Hello World!";
-//inner HTML for content of the element
-</script>
-
-</body>
-</html>
-document.createElement(element)
-document.removeChild(element)
-document.appendChild(element)
-document.replaceChild(new, old)
-const element = document.getElementsByTagName("p");// all <p> elemnts\
-const x = document.getElementsByClassName("intro");// class names
-//css
-{/* <html>
-<body>
-
-<p id="p2">Hello World!</p>
-
-<script>
-document.getElementById("p2").style.color = "blue";
-</script>
-
-</body>
-</html> */}
-// <!DOCTYPE html>
-// <html>
-// <style>
-// #container {
-//   width: 400px;
-//   height: 400px;
-//   position: relative;
-//   background: yellow;
+// function myFunction(value, index, array) {
+//   txt += value + "<br>";
 // }
-// #animate {
-//   width: 50px;
-//   height: 50px;
-//   position: absolute;
-//   background-color: red;
+// const myArr = [1, 2, 3, 4, 5, 6];
+// const newArr = myArr.flatMap((x) => x * 2);//the same as java
+// const numbers3 = [45, 4, 9, 16, 25];
+// let allOver18 = numbers3.every(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
 // }
-// </style>
-// <body>
+// const numbers4 = [45, 4, 9, 16, 25];
+// let someOver18 = numbers4.some(myFunction);
 
-// <p><button onclick="myMove()">Click Me</button></p> 
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+// //types arrays
+// const myArr2 = new Uint8Array(5);
+// const myArr3 = Uint8Array.of(0,1,2,3,4);
+// //typed arrays to handle binary data
+// const myArr4 = new Uint8ClampedArray(10);
+// //sets only have unique values
+// const letters = new Set(["a","b","c"]);
+// const letters2 = new Set();
+// letters instanceof Set;
+// //maps
+// const fruits = new Map([
+//   ["apples", 500],
+//   ["bananas", 300],
+//   ["oranges", 200]
+// ]);
+// //==
+// const fruit2 = new Map();
 
-// <div id ="container">
-//   <div id ="animate"></div>
-// </div>
+// // Set Map Values
+// fruits.set("apples", 500);
+// fruits.set("bananas", 300);
+// fruits.set("oranges", 200);
+// fruits.get("apples");
+// //events
+// <button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
+// //function constructor
+// const myFunction = new Function("a", "b", "return a * b");
+// //hoisting
+// myFunction(5);
 
-// <script>
-// function myMove() {
-//   let id = null;
-//   const elem = document.getElementById("animate");   
-//   let pos = 0;
-//   clearInterval(id);
-//   id = setInterval(frame, 5);
-//   function frame() {
-//     if (pos == 350) {
-//       clearInterval(id);
-//     } else {
-//       pos++; 
-//       elem.style.top = pos + "px"; 
-//       elem.style.left = pos + "px"; 
-//     }
+// function myFunction(y) {
+//   return y * y;
+// }
+// //self invoking functions
+// (function () {
+//   let x = "Hello!!";
+// })();//functions are objects
+// //this here is used as global object
+// const person = {
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
 //   }
 // }
+// const person1 = {
+//   firstName:"John",
+//   lastName: "Doe"
+// }
+// const person2 = {
+//   firstName:"Mary",
+//   lastName: "Doe"
+// }
+
+// person.fullName.call(person1);//call used to use a method belonging to another object
+// const person = {
+//   fullName: function(city, country) {
+//     return this.firstName + " " + this.lastName + "," + city + "," + country;
+//   }
+// }
+
+// const person1 = {
+//   firstName:"John",
+//   lastName: "Doe"
+// }
+// person.fullName.apply(person1);//takes argyments as array
+// person.fullName.call(person1, "Oslo", "Norway");
+// //objects 
+// const person = new Object({
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// });
+// function Person(first, last, age, eye) {//same as java
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+// }
+// //destructering
+// let {firstName, lastName} = person;
+// Object.assign(person1, person2);//iterations
+// let text = Object.entries(person);//
+// //getter and setter are the same as java
+// class Car {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+// }
+// let myPromise = new Promise(function(myResolve, myReject) {//producing and consuming code
+//   myResolve(); // when successful
+//   myReject();  // when error
+// });
+
+// // "Consuming Code" (Must wait for a fulfilled Promise)
+// myPromise.then(
+//   function(value) { /* code if successful */ },
+//   function(error) { /* code if some error */ }
+// );
+// //promise and async
+// async function myFunction() {
+//   return "Hello";
+// }
+// async function myFunction() {
+//   return "Hello";
+// }
+// myFunction().then(
+//   function(value) {myDisplayer(value);},
+//   function(error) {myDisplayer(error);}
+// );
+// let value = await promise;//only able to be used in asynch
+// async function myDisplay() {
+//   let myPromise = new Promise(function(resolve, reject) {
+//     resolve("!JOFJAISFJLDJ");
+//   });
+//   document.getElementById("demo").innerHTML = await myPromise;
+// }
+
+// myDisplay();
+// async function myDisplay() {
+//   let myPromise = new Promise(function(resolve) {
+//     setTimeout(function() {resolve("DSASDSA");}, 3000);
+//   });//timeout
+//   document.getElementById("demo").innerHTML = await myPromise;
+// }
+
+// myDisplay();
+
+// //The HTML elements as objects
+// //The properties of all HTML elements
+// //The methods to access all HTML elements
+// //The events for all HTML element
+// <html>
+// <body>
+
+// <p id="demo"></p>
+
+// <script>
+// document.getElementById("demo").innerHTML = "Hello World!";
+// //inner HTML for content of the element
 // </script>
 
 // </body>
 // </html>
-//java script events
-{/* <script>
-document.getElementById("myBtn").onclick = displayDate;
-</script> */}
-{/* <body onload="checkCookies()"></body> */}
-{/* <input type="text" id="fname" oninput="upperCase()"></input> */}
-// element.addEventListener(event, function, useCapture);//event listener
-// element.addEventListener("click", myFunction);
-// element.addEventListener("click", mySecondFunction);
+// document.createElement(element)
+// document.removeChild(element)
+// document.appendChild(element)
+// document.replaceChild(new, old)
+// const element = document.getElementsByTagName("p");// all <p> elemnts\
+// const x = document.getElementsByClassName("intro");// class names
+// //css
+// {/* <html>
+// <body>
 
-//navigating through nodes
-{/* <html>
-<body>
+// <p id="p2">Hello World!</p>
 
-<h1 id="id01">My First Page</h1>
-<p id="id02"></p>
+// <script>
+// document.getElementById("p2").style.color = "blue";
+// </script>
 
-<script>
-document.getElementById("id02").innerHTML = document.getElementById("id01").firstChild.nodeValue;
-</script>
+// </body>
+// </html> */}
+// // <!DOCTYPE html>
+// // <html>
+// // <style>
+// // #container {
+// //   width: 400px;
+// //   height: 400px;
+// //   position: relative;
+// //   background: yellow;
+// // }
+// // #animate {
+// //   width: 50px;
+// //   height: 50px;
+// //   position: absolute;
+// //   background-color: red;
+// // }
+// // </style>
+// // <body>
 
-</body>
-</html> */}
-{/* <html>
-<body>
+// // <p><button onclick="myMove()">Click Me</button></p> 
 
-<h1 id="id01">My First Page</h1>
-<p id="id02">Hello!</p>
+// // <div id ="container">
+// //   <div id ="animate"></div>
+// // </div>
 
-<script>
-document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
-</script>
+// // <script>
+// // function myMove() {
+// //   let id = null;
+// //   const elem = document.getElementById("animate");   
+// //   let pos = 0;
+// //   clearInterval(id);
+// //   id = setInterval(frame, 5);
+// //   function frame() {
+// //     if (pos == 350) {
+// //       clearInterval(id);
+// //     } else {
+// //       pos++; 
+// //       elem.style.top = pos + "px"; 
+// //       elem.style.left = pos + "px"; 
+// //     }
+// //   }
+// // }
+// // </script>
 
-</body>
-</html> */}
+// // </body>
+// // </html>
+// //java script events
+// {/* <script>
+// document.getElementById("myBtn").onclick = displayDate;
+// </script> */}
+// {/* <body onload="checkCookies()"></body> */}
+// {/* <input type="text" id="fname" oninput="upperCase()"></input> */}
+// // element.addEventListener(event, function, useCapture);//event listener
+// // element.addEventListener("click", myFunction);
+// // element.addEventListener("click", mySecondFunction);
+
+// //navigating through nodes
+// {/* <html>
+// <body>
+
+// <h1 id="id01">My First Page</h1>
+// <p id="id02"></p>
+
+// <script>
+// document.getElementById("id02").innerHTML = document.getElementById("id01").firstChild.nodeValue;
+// </script>
+
+// </body>
+// </html> */}
+// {/* <html>
+// <body>
+
+// <h1 id="id01">My First Page</h1>
+// <p id="id02">Hello!</p>
+
+// <script>
+// document.getElementById("id02").innerHTML = document.getElementById("id01").childNodes[0].nodeValue;
+// </script>
+
+// </body>
+// </html> */}
--- study.php
+++ study.php
@@ -1,14 +1,281 @@
+<!-- <?php
+// echo phpversion();
+// $p = 1;
+// $isntthisaglobal = "th";
+// if($p ==1 )
+//     {
+//         echo "huh";
+//     }
+// $cars = array("this","is","not","a","car");
+// foreach($cars as $x)
+//     {
+//         echo $_SERVER['PHP_SELF'];
+//     }
+?> -->
+//$GLOBALS is an array that contains all global variables.
+//$_SERVER is a PHP super global variable which holds information about headers, paths, and script locations..
+//$_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.
+/*$GLOBALS
+$_SERVER
+$_REQUEST
+$_POST
+$_GET
+$_FILES
+$_ENV
+$_COOKIE
+$_SESSION */
+define(name, value);//constant
+//or just use const
+__CLASS__	If used inside a class, the class name is returned.
+__DIR__	The directory of the file.
+__FILE__	The file name including the full path.
+__FUNCTION__	If inside a function, the function name is returned.
+__LINE__	The current line number.
+__METHOD__	If used inside a function that belongs to a class, both class and function name is returned.
+__NAMESPACE__	If used inside a namespace, the name of the namespace is returned.
+__TRAIT__	If used inside a trait, the trait name is returned.
+ClassName::class	Returns the name of the specified class and the name of the namespace, if any.
+===	Identity
+!==	Non-identity
+.	Concatenation
+.=	Concatenation assignment	$txt1 .= $txt2
+echo preg_match_all($pattern, $str);
+<!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail:
+<input type="text" name="email">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+Website:
+<input type="text" name="website">
+<span class="error"><?php echo $websiteErr;?></span>
+<br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+<br><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other
+<span class="error">* <?php echo $genderErr;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit">
+
+</form>
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+  
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+  
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+  
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+<!-- }
 <?php
-echo phpversion();
-$p = 1;
-$isntthisaglobal = "th";
-if($p ==1 )
-    {
-        echo "huh";
-    }
-$cars = array("this","is","not","a","car");
-foreach($cars as $x)
-    {
-        echo $_SERVER['PHP_SELF'];
-    }
-?>
+// $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// echo fgets($myfile);
+// fclose($myfile);
+// ?>
+// <?php
+// $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+// $txt = "John Doe\n";
+// fwrite($myfile, $txt);
+// $txt = "Jane Doe\n";
+// fwrite($myfile, $txt);
+// fclose($myfile);
+?> -->
+<!-- <?php
+// $target_dir = "uploads/";
+// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $uploadOk = 1;
+// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// if(isset($_POST["submit"])) {
+//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//   if($check !== false) {
+//     echo "File is an image - " . $check["mime"] . ".";
+//     $uploadOk = 1;
+//   } else {
+//     echo "File is not an image.";
+//     $uploadOk = 0;
+//   }
+// }
+// ?> -->
+// <!-- <?php
+// $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+// echo json_encode($age);
+// ?>
+// <?php
+// $cookie_name = "user";
+// $cookie_value = "John Doe";
+// setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+// ?>
+// <html>
+// <body> -->
+// <!-- 
+// <?php
+// if(!isset($_COOKIE[$cookie_name])) {
+//   echo "Cookie named '" . $cookie_name . "' is not set!";
+// } else {
+//   echo "Cookie '" . $cookie_name . "' is set!<br>";
+//   echo "Value is: " . $_COOKIE[$cookie_name];
+// }
+// ?>
+
+</body>
+</html>
+<?php -->
+
+// $_SESSION["favcolor"] = "green";
+// $_SESSION["favanimal"] = "cat";
+// echo "Session variables are set.";
+// ?>
+// <?php
+// class Fruit {
+
+//   public $name;
+//   public $color;
+
+//   function set_name($name) {
+//     $this->name = $name;
+//   }
+//   function get_name() {
+//     return $this->name;
+//   }
+// }
+// ?>
+// <?php
+// class Fruit {
+//   public $name;
+//   public $color;
+
+//   function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+//   function get_name() {
+//     return $this->name;
+//   }
+//   function get_color() {
+//     return $this->color;
+//   }
+// }
+
+// $apple = new Fruit("Apple", "red");
+// echo $apple->get_name();
+// // echo "<br>";
+// // echo $apple->get_color();
+// // ?>
+// <!-- <?php
+// abstract class ParentClass {
+//   abstract public function someMethod1();
+//   abstract public function someMethod2($name, $color);
+//   abstract public function someMethod3() : string;
+// }
+// ?> -->
+// <!-- <?php
+// class Fruit {
+//   public $name;
+//   public $color;
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+//   public function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}.";
+//   }
+// }
+
+// class Strawberry extends Fruit {
+//   public function message() {
+//     echo "Am I a fruit or a berry? ";
+//   }
+// }
+// $strawberry = new Strawberry("Strawberry", "red");
+// $strawberry->message();
+// $strawberry->intro();
+// ?>
+// <?php
+// interface InterfaceName {
+//   public function someMethod1();
+//   public function someMethod2($name, $color);
+//   public function someMethod3() : string;
+// }
+// ?> -->
+// <!-- <?php
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+
+// $conn = new mysqli($servername, $username, $password);
+
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+// $sql = "CREATE DATABASE myDB";
+// if ($conn->query($sql) === TRUE) {
+//   echo "Database created successfully";
+// } else {
+//   echo "Error creating database: " . $conn->error;
+// }
+// ?> -->
+// <--<?php
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+// $dbname = "myDB";
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "Record updated successfully";
+// } else {
+//   echo "Error updating record: " . $conn->error;
+// }
+
+// $conn->close();
+// ?> -->
